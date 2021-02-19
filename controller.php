890aff9d663c031497786d8ec395659a3db573c3
a3020 <a3020@fastmail.com>
2021-02-19 18:22:56 +0100

    Version 1.0

--- controller.php
+++ controller.php
@@ -10,7 +10,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'auto_login';
     protected $appVersionRequired = '5.7.4';
-    protected $pkgVersion = '0.9.4';
+    protected $pkgVersion = '1.0';
 
     protected $single_pages = array(
         '/dashboard/system/registration/auto_login' => array(
--- src/AutoLogin/AutoLogin.php
+++ src/AutoLogin/AutoLogin.php
@@ -3,7 +3,7 @@ namespace Concrete\Package\AutoLogin\Src\AutoLogin;
 
 use Concrete\Authentication\Concrete\Controller;
 use Concrete\Core\Utility\IPAddress;
-use Concrete\Package\RestrictLogin\Src\RestrictLogin;
+use Symfony\Component\HttpFoundation\IpUtils;
 use Config;
 use Core;
 use Package;
@@ -13,8 +13,6 @@ class AutoLogin
 {
     public function boot()
     {
-        $entries = Config::get('auto_login.entries');
-
         if (User::isLoggedIn()) {
             return false;
         }
@@ -24,19 +22,19 @@ class AutoLogin
         $ip = $iph->getRequestIP();
         $ip = $ip->getIp(IPAddress::FORMAT_IP_STRING);
 
-        // Couldn't retrieve IP or IP is not in the config file.
-        if ($ip === null or !isset($entries[$ip])) {
+        // Couldn't retrieve IP
+        if ($ip === null) {
             return false;
         }
 
-        $entry = $entries[$ip];
+        $ip_entry = $this->getIPMatch($ip);
 
-        // Auto login for this IP is currently disabled
-        if (!$entry['enabled']) {
+        // No active IP entry found, don't login.
+        if (!$ip_entry) {
             return false;
         }
 
-        $u = User::getByUserID($entry['uID']);
+        $u = User::getByUserID($ip_entry['uID']);
 
         // Invalid or non existing user.
         if (!is_object($u) || !($u instanceof User) || $u->isError()) {
@@ -48,22 +46,49 @@ class AutoLogin
          * and the users's IP is not allowed to login. ("Restrict Login" takes precedence)
          */
         $p = Package::getByHandle('restrict_login');
-        
-        if ($p && $p->isPackageInstalled() && class_exists("Concrete\Package\RestrictLogin\Src\RestrictLogin\RestrictLogin")) {
-            $rl = new RestrictLogin\RestrictLogin();
+
+        if ($p && $p->isPackageInstalled() && class_exists("\Concrete\Package\RestrictLogin\Src\RestrictLogin\RestrictLogin")) {
+            $rl = new \Concrete\Package\RestrictLogin\Src\RestrictLogin\RestrictLogin();
             if (method_exists($rl, "getAllowedIPs")) {
                 $allowed_ips = $rl->getAllowedIPs();
 
-                if (count($allowed_ips) && !in_array($ip, $allowed_ips)) {
+                if (count($allowed_ips) && !$rl->checkIP($ip, $allowed_ips)) {
                     return false;
                 }
             }
         }
 
-        $u = User::loginByUserID($entry['uID']);
+        $u = User::loginByUserID($ip_entry['uID']);
 
         // Use default C5 authentication.
         $concrete = new Controller();
         $concrete->completeAuthentication($u);
+    }
+
+    /**
+     * Returns config entry if IP matches.
+     *
+     * @param string $user_ip
+     *
+     * @return bool|array
+     */
+    public function getIPMatch($user_ip)
+    {
+        $entries = Config::get('auto_login.entries');
+
+        foreach ($entries as $ip => $ip_entry) {
+            // Auto login for this IP is currently disabled
+            if (!$ip_entry['enabled']) {
+                continue;
+            }
+
+            $match = IpUtils::checkIp($user_ip, $ip);
+
+            if ($match) {
+                return $ip_entry;
+            }
+        }
+
+        return false;
     }
 }
